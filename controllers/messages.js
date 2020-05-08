87d84bbefc2369ba6aae2c0d719ec99967be2152
mohamed <ragabmohame6@gmail.com>
2020-05-08 22:19:28 +0200

    message route: refactor delete

--- controllers/messages.js
+++ controllers/messages.js
@@ -40,10 +40,24 @@ exports.updateMessage = async function(req, res, next) {
 
 exports.deleteMessage = async function (req, res, next) {
     try {
-        let foundMessage = await db.Message.findById(req.params.message_id);
-        await foundMessage.remove();
-        return res.status(200).json(foundMessage);
+        let foundMessage = await db.Message.findById(req.params.message_id).populate("user", {
+            id: true
+        });
+
+        if(foundMessage.user.id === req.params.id) {
+            await foundMessage.remove();
+            return res.status(200).json(foundMessage);
+        } else {
+            return next({
+                status: 401,
+                message: "You don't have permission to do that"
+            });
+        }
+
     } catch (err) {
-        next(err);
+        return next({
+            status: 401,
+            message: "You don't have permission to do that"
+        });
     }
 };
