aa6755d62df09ea2b8f9d8dd96dae83eb66ef97a
mohamed <ragabmohame6@gmail.com>
2020-05-08 04:29:58 +0200

    error handler

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express    = require('express'),
-      app        = express(),
-      bodyParser = require('body-parser'),
-      cors       = require('cors');
+const express      = require('express'),
+      app          = express(),
+      cors         = require('cors'),
+      bodyParser   = require('body-parser'),
+      errorHandler = require('./controllers/error');
 
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
     console.log(`Server is starting on port ${PORT}`);
-})
+});
