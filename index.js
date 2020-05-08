a9597a0bb565e3439c0799f7884720933b014405
mohamed <ragabmohame6@gmail.com>
2020-05-08 17:54:04 +0200

    message route: get all messages

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express       = require('express'),
       app           = express(),
       cors          = require('cors'),
       bodyParser    = require('body-parser'),
+      db            = require('./models'),
       errorHandler  = require('./controllers/error'),
       authRoutes    = require('./routes/auth'),
       messageRoutes = require('./routes/messages');
@@ -14,6 +15,19 @@ app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users/:id/messages', messageRoutes);
+
+app.get('/api/messages', async function(req, res, next) {
+   try {
+       let messages = await db.Message.find()
+           .sort({createdAt: -1})
+           .populate("user", {
+               username: true,
+           });
+       return res.status(200).json(messages);
+   } catch (err) {
+       next(err);
+   }
+});
 
 app.use((req, res, next) => {
     let err = new Error("Not Found");
